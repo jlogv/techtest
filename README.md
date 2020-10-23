3b411582efb50c5b9ec97b88c66a94215e3d4a80
Logvinov Alecksey <alecksey@yandex.ru>
2020-10-23 06:32:57 +0300

    add actix

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # techtest
-Test task
+Test task.
+
+This code shows how the task can be solved with web framework. I use actix-web framework to implement simple REST API with only one link /api/calc.
+So, we send the json request to web server, for example:
+{"a": false, "b": true, "c": true, "d": 30.0, "e": 10, "f": 10}
+to http://127.0.0.1:8080/api/calc
+and receive the result.
+
+Result can be like json this answer or just error text, if error was occuped.
+
+Using the Insomnia tool it`s can be tested.
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,32 @@
+#![allow(unused_imports)]
 #![allow(unused_variables)]
 #![allow(unused_assignments)]
+#![allow(dead_code)]
+
+use serde::{Serialize, Deserialize};
+use actix_web::{HttpServer, dev, web, error, http, middleware, App, Error, HttpMessage, HttpRequest,
+                HttpResponse};
+use log::info;
+use std::process::Command;
+use std::io::{self, Write};
+
+extern crate simple_logger;
+
+#[derive(Deserialize)]
+pub struct Req {
+    a: bool,
+    b: bool,
+    c: bool,
+    d: f32,
+    e: i32,
+    f: i32
+}
+
+#[derive(Serialize, Deserialize)]
+struct Res {
+    k : f32,
+    h : String
+}
 
 enum Kind {
     M, P, T, None
@@ -13,6 +40,64 @@ fn calc (h : &Kind, d: f32, e: i32, f : i32) -> f32
         Kind::T => d - (d * f as f32 / 30.0),
         _ => 0.0
     }
+}
+
+fn exec_res(a : bool, b : bool, c : bool, d : f32, e : i32, f : i32) -> Option<Res>
+{
+    let mut k : f32 = 0.0;
+
+    let expression_set = (a,b,c);
+
+    let mut h: Kind = Kind::None;
+
+    match expression_set {
+        (a,b,c) if (a && b && !c) =>
+            {
+                h = Kind::M;
+            },
+        (a,b,c) if (a && b && c) =>
+            {
+                h = Kind::P;
+            },
+        (a,b,c) if (!a && b && c) =>
+            {
+                h = Kind::T;
+            },
+        _=> return None
+    }
+
+    k = calc(&h, d, e, f);
+
+    if let Kind::P = h {
+        k = 2.0 * d + (d * e as f32 / 100.0);
+    }
+
+    match expression_set {
+        (a,b,c) if (a && b && !c) => {
+            h = Kind::T;
+            k = calc(&h, d, e, f);
+        },
+        (a,b,c) if (a && !b && c) => {
+            h = Kind::M;
+            k = calc(&h, d, e, f);
+        },
+        _=> {},
+    }
+
+    if let Kind::M = h {
+        k = f as f32 + d + (d * e as f32 / 100.0);
+    }
+
+    let mut kind_text = String::from("");
+
+    match h {
+        Kind::M => kind_text = String::from("M"),
+        Kind::P => kind_text = String::from("P"),
+        Kind::T => kind_text = String::from("T"),
+        _ => ()
+    }
+
+    Some(Res{k: k, h:kind_text})
 }
 
 fn exec(a : bool, b : bool, c : bool, d : f32, e : i32, f : i32) -> Option<f32>
@@ -61,10 +146,44 @@ fn exec(a : bool, b : bool, c : bool, d : f32, e : i32, f : i32) -> Option<f32>
         k = f as f32 + d + (d * e as f32 / 100.0);
     }
 
+    let mut kind_text = String::from("");
+
+    match h {
+        Kind::M => kind_text = String::from("M"),
+        Kind::P => kind_text = String::from("P"),
+        Kind::T => kind_text = String::from("T"),
+        _ => ()
+    }
+
     Some(k)
 }
 
-fn main() {
+pub async fn calculate(req: web::Json<Req>) -> Result<HttpResponse, error::Error>
+{
+    let c = exec_res(req.a, req.b, req.c, req.d, req.e, req.f);
+    if c.is_some() {
+        Ok(HttpResponse::Ok().json(c))
+    }
+    else {
+        Ok(HttpResponse::Ok().body("Error"))
+    }
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+
+    let srv = HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .service(web::scope("/api")
+                .service(web::resource("/calc").route(web::post().to(calculate)))
+            )
+        })
+        .keep_alive(Some(0))
+        .bind("127.0.0.1:8080")?
+        .run();
+
+    /*
     let a : bool = true;
     let b : bool = true;
     let c : bool = true;
@@ -77,6 +196,8 @@ fn main() {
         Some(k) => println!("k: {}", k),
         None => println!("error")
     }
+    */
+    srv.await
 }
 
 #[test]
