77ae882d1660196bf28178d32762ba75486be691
Logvinov Alecksey <alecksey@yandex.ru>
2020-10-23 04:07:06 +0300

    add test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#![allow(unused_variables)]
+#![allow(unused_assignments)]
+
 enum Kind {
     M, P, T, None
 }
@@ -12,15 +15,8 @@ fn calc (h : &Kind, d: f32, e: i32, f : i32) -> f32
     }
 }
 
-fn main() {
-
-    let a : bool = true;
-    let b : bool = true;
-    let c : bool = true;
-    let d : f32 = 0.0;
-    let e : i32 = 0;
-    let f : i32 = 0;
-
+fn exec(a : bool, b : bool, c : bool, d : f32, e : i32, f : i32) -> Option<f32>
+{
     let mut k : f32 = 0.0;
 
     let expression_set = (a,b,c);
@@ -30,20 +26,17 @@ fn main() {
     match expression_set {
         (a,b,c) if (a && b && !c) =>
             {
-                println!("first");
                 h = Kind::M;
             },
         (a,b,c) if (a && b && c) =>
             {
-                println!("second");
                 h = Kind::P;
             },
         (a,b,c) if (!a && b && c) =>
             {
-                println!("third");
                 h = Kind::T;
             },
-        _=> println!("error"),
+        _=> return None
     }
 
     k = calc(&h, d, e, f);
@@ -54,20 +47,69 @@ fn main() {
 
     match expression_set {
         (a,b,c) if (a && b && !c) => {
-            println!("first too");
             h = Kind::T;
+            k = calc(&h, d, e, f);
         },
         (a,b,c) if (a && !b && c) => {
-            println!("second too");
             h = Kind::M;
+            k = calc(&h, d, e, f);
         },
         _=> {},
     }
 
-    k = calc(&h, d, e, f);
-
-    if let Kind::P = h {
+    if let Kind::M = h {
         k = f as f32 + d + (d * e as f32 / 100.0);
     }
 
+    Some(k)
+}
+
+fn main() {
+    let a : bool = true;
+    let b : bool = true;
+    let c : bool = true;
+    let d : f32 = 0.0;
+    let e : i32 = 0;
+    let f : i32 = 0;
+
+    let k = exec(false, false, false, 30.0, 10, 10);
+    match k {
+        Some(k) => println!("k: {}", k),
+        None => println!("error")
+    }
+}
+
+#[test]
+fn test_a_b_c () {
+    assert_eq!(exec(true, true, true, 30.0, 10, 10), Some(63.0));
+}
+
+#[test]
+fn test_not_a_b_c () {
+    assert_eq!(exec(false, true, true, 30.0, 10, 10), Some(20.0));
+}
+
+#[test]
+fn test_a_not_b_c () {
+    assert_eq!(exec(true, false, true, 30.0, 10, 10), None);
+}
+
+#[test]
+fn test_a_b_not_c () {
+    assert_eq!(exec(true, true, false, 30.0, 10, 10), Some(20.0));
+}
+
+#[test]
+fn test_not_a_not_b_c () {
+    assert_eq!(exec(false, false, true, 30.0, 10, 10), None);
+}
+
+#[test]
+fn test_not_a_b_not_c () {
+    assert_eq!(exec(false, true, false, 30.0, 10, 10), None);
+}
+
+#[test]
+fn test_not_a_not_b_not_c () {
+    assert_eq!(exec(false, false, false, 30.0, 10, 10), None);
 }
